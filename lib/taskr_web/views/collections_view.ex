a0babbd54db1510c6c893cb628814ef5b38cc17d
taytestokes <taytestokes9328@gmail.com>
2022-09-19 11:01:32 -0600

    Updates collection show page

--- lib/taskr_web/views/collections_view.ex
+++ lib/taskr_web/views/collections_view.ex
@@ -3,8 +3,8 @@ defmodule TaskrWeb.CollectionsView do
 
   def get_task_toggle_button_styles(task) do
     case task.completed do
-       false -> "flex h-5 w-5 rounded-md border-2 border-black"
-       _ -> "flex items-center justify-center h-5 w-5 rounded-md border-2 border-black bg-black"
+       false -> "flex h-6 w-6 rounded-full border-2 border-black"
+       _ -> "flex items-center justify-center h-6 w-6 rounded-full border-2 border-black bg-black"
     end
   end
 
